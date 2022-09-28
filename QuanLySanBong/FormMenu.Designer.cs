9d467e3e467934b0e4efe9e72c9e50f348f16a8a
acer <nctam.25032002@gmail.com>
2022-09-28 19:44:52 +0700

    editmenu

--- QuanLySanBong/FormMenu.Designer.cs
+++ QuanLySanBong/FormMenu.Designer.cs
@@ -63,7 +63,6 @@ namespace QuanLySanBong
             // 
             // pictureBox12
             // 
-            this.pictureBox12.Image = global::QuanLySanBong.Properties.Resources.khungthanh;
             this.pictureBox12.Location = new System.Drawing.Point(2, 5);
             this.pictureBox12.Name = "pictureBox12";
             this.pictureBox12.Size = new System.Drawing.Size(1085, 677);
@@ -73,8 +72,7 @@ namespace QuanLySanBong
             // 
             // pictureBox9
             // 
-            this.pictureBox9.Image = global::QuanLySanBong.Properties.Resources.nhanvien;
-            this.pictureBox9.Location = new System.Drawing.Point(648, 299);
+            this.pictureBox9.Location = new System.Drawing.Point(676, 299);
             this.pictureBox9.Name = "pictureBox9";
             this.pictureBox9.Size = new System.Drawing.Size(83, 69);
             this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -84,17 +82,17 @@ namespace QuanLySanBong
             // btnQLNV
             // 
             this.btnQLNV.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.btnQLNV.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnQLNV.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.btnQLNV.Location = new System.Drawing.Point(699, 299);
+            this.btnQLNV.Location = new System.Drawing.Point(756, 299);
             this.btnQLNV.Name = "btnQLNV";
-            this.btnQLNV.Size = new System.Drawing.Size(250, 69);
+            this.btnQLNV.Size = new System.Drawing.Size(211, 69);
             this.btnQLNV.TabIndex = 39;
             this.btnQLNV.Text = "Quản Lý Nhân Viên";
             this.btnQLNV.UseVisualStyleBackColor = true;
             // 
             // pictureBox8
             // 
-            this.pictureBox8.Image = global::QuanLySanBong.Properties.Resources.dichvu;
             this.pictureBox8.Location = new System.Drawing.Point(126, 572);
             this.pictureBox8.Name = "pictureBox8";
             this.pictureBox8.Size = new System.Drawing.Size(74, 69);
@@ -105,6 +103,7 @@ namespace QuanLySanBong
             // btnQLDV
             // 
             this.btnQLDV.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.btnQLDV.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnQLDV.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.btnQLDV.Location = new System.Drawing.Point(180, 572);
             this.btnQLDV.Name = "btnQLDV";
@@ -115,7 +114,6 @@ namespace QuanLySanBong
             // 
             // pictureBox7
             // 
-            this.pictureBox7.Image = global::QuanLySanBong.Properties.Resources.hoadon;
             this.pictureBox7.Location = new System.Drawing.Point(126, 440);
             this.pictureBox7.Name = "pictureBox7";
             this.pictureBox7.Size = new System.Drawing.Size(77, 69);
@@ -126,6 +124,7 @@ namespace QuanLySanBong
             // btnQLHoaDon
             // 
             this.btnQLHoaDon.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.btnQLHoaDon.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnQLHoaDon.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.btnQLHoaDon.Location = new System.Drawing.Point(180, 440);
             this.btnQLHoaDon.Name = "btnQLHoaDon";
@@ -136,8 +135,7 @@ namespace QuanLySanBong
             // 
             // pictureBox6
             // 
-            this.pictureBox6.Image = global::QuanLySanBong.Properties.Resources.battay;
-            this.pictureBox6.Location = new System.Drawing.Point(648, 424);
+            this.pictureBox6.Location = new System.Drawing.Point(676, 424);
             this.pictureBox6.Name = "pictureBox6";
             this.pictureBox6.Size = new System.Drawing.Size(83, 69);
             this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -147,27 +145,28 @@ namespace QuanLySanBong
             // btnQLKH
             // 
             this.btnQLKH.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.btnQLKH.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnQLKH.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.btnQLKH.Location = new System.Drawing.Point(699, 424);
+            this.btnQLKH.Location = new System.Drawing.Point(756, 424);
             this.btnQLKH.Name = "btnQLKH";
-            this.btnQLKH.Size = new System.Drawing.Size(250, 69);
+            this.btnQLKH.Size = new System.Drawing.Size(211, 69);
             this.btnQLKH.TabIndex = 33;
             this.btnQLKH.Text = "Quản Lý Khách Hàng";
             this.btnQLKH.UseVisualStyleBackColor = true;
             // 
             // pictureBox5
             // 
-            this.pictureBox5.Image = global::QuanLySanBong.Properties.Resources.sanbong;
             this.pictureBox5.Location = new System.Drawing.Point(126, 290);
             this.pictureBox5.Name = "pictureBox5";
             this.pictureBox5.Size = new System.Drawing.Size(77, 69);
-            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox5.TabIndex = 32;
             this.pictureBox5.TabStop = false;
             // 
             // btnQLSan
             // 
             this.btnQLSan.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.btnQLSan.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnQLSan.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.btnQLSan.Location = new System.Drawing.Point(180, 290);
             this.btnQLSan.Name = "btnQLSan";
@@ -178,8 +177,7 @@ namespace QuanLySanBong
             // 
             // pictureBox4
             // 
-            this.pictureBox4.Image = global::QuanLySanBong.Properties.Resources.thongke;
-            this.pictureBox4.Location = new System.Drawing.Point(648, 176);
+            this.pictureBox4.Location = new System.Drawing.Point(676, 176);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(83, 69);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -189,17 +187,17 @@ namespace QuanLySanBong
             // btnThongKe
             // 
             this.btnThongKe.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.btnThongKe.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnThongKe.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.btnThongKe.Location = new System.Drawing.Point(697, 176);
+            this.btnThongKe.Location = new System.Drawing.Point(756, 176);
             this.btnThongKe.Name = "btnThongKe";
-            this.btnThongKe.Size = new System.Drawing.Size(252, 69);
+            this.btnThongKe.Size = new System.Drawing.Size(211, 69);
             this.btnThongKe.TabIndex = 29;
             this.btnThongKe.Text = "Thống Kê Doanh Thu";
             this.btnThongKe.UseVisualStyleBackColor = true;
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Image = global::QuanLySanBong.Properties.Resources.dothethao;
             this.pictureBox3.Location = new System.Drawing.Point(126, 176);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(74, 69);
@@ -210,29 +208,30 @@ namespace QuanLySanBong
             // btnDichVuThanhToan
             // 
             this.btnDichVuThanhToan.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.btnDichVuThanhToan.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDichVuThanhToan.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.btnDichVuThanhToan.Location = new System.Drawing.Point(175, 176);
             this.btnDichVuThanhToan.Name = "btnDichVuThanhToan";
-            this.btnDichVuThanhToan.Size = new System.Drawing.Size(231, 69);
+            this.btnDichVuThanhToan.Size = new System.Drawing.Size(225, 69);
             this.btnDichVuThanhToan.TabIndex = 27;
             this.btnDichVuThanhToan.Text = "Dịch Vụ Và Thanh Toán";
             this.btnDichVuThanhToan.UseVisualStyleBackColor = true;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Image = global::QuanLySanBong.Properties.Resources.check;
-            this.pictureBox2.Location = new System.Drawing.Point(697, 37);
+            this.pictureBox2.Location = new System.Drawing.Point(676, 37);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(111, 69);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.Size = new System.Drawing.Size(83, 69);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 26;
             this.pictureBox2.TabStop = false;
             // 
             // btnNhanSan
             // 
             this.btnNhanSan.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.btnNhanSan.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnNhanSan.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.btnNhanSan.Location = new System.Drawing.Point(735, 37);
+            this.btnNhanSan.Location = new System.Drawing.Point(717, 37);
             this.btnNhanSan.Name = "btnNhanSan";
             this.btnNhanSan.Size = new System.Drawing.Size(250, 69);
             this.btnNhanSan.TabIndex = 25;
@@ -241,19 +240,19 @@ namespace QuanLySanBong
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::QuanLySanBong.Properties.Resources.lich;
-            this.pictureBox1.Location = new System.Drawing.Point(103, 37);
+            this.pictureBox1.Location = new System.Drawing.Point(126, 37);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(117, 69);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.Size = new System.Drawing.Size(77, 69);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 24;
             this.pictureBox1.TabStop = false;
             // 
             // btnDatSan
             // 
             this.btnDatSan.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.btnDatSan.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDatSan.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.btnDatSan.Location = new System.Drawing.Point(102, 37);
+            this.btnDatSan.Location = new System.Drawing.Point(196, 37);
             this.btnDatSan.Name = "btnDatSan";
             this.btnDatSan.Size = new System.Drawing.Size(204, 69);
             this.btnDatSan.TabIndex = 23;
@@ -264,7 +263,7 @@ namespace QuanLySanBong
             // 
             this.btnThoat.BackColor = System.Drawing.Color.Salmon;
             this.btnThoat.Font = new System.Drawing.Font("Microsoft Tai Le", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnThoat.Location = new System.Drawing.Point(697, 572);
+            this.btnThoat.Location = new System.Drawing.Point(726, 572);
             this.btnThoat.Name = "btnThoat";
             this.btnThoat.Size = new System.Drawing.Size(196, 69);
             this.btnThoat.TabIndex = 22;
