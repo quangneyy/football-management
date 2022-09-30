4c4201051eaab401cf0fc7dda87679f7531f2b4e
PhamHoaiNam <phamhoainambt86@gmail.com>
2022-09-30 23:41:56 +0700

    update form QLdichvu

--- QuanLySanBong/FormQLdichvu.Designer.cs
+++ QuanLySanBong/FormQLdichvu.Designer.cs
@@ -29,20 +29,26 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dvgthontin = new System.Windows.Forms.DataGridView();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.txtcp = new System.Windows.Forms.TextBox();
+            this.txttendv = new System.Windows.Forms.TextBox();
+            this.txtmadv = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnthem = new System.Windows.Forms.Button();
+            this.btnxoa = new System.Windows.Forms.Button();
+            this.btnsua = new System.Windows.Forms.Button();
+            this.btnthoat = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgthontin)).BeginInit();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.dvgthontin);
             this.groupBox1.Location = new System.Drawing.Point(56, 47);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(540, 261);
@@ -50,74 +56,136 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Thông tin";
             // 
-            // dataGridView1
+            // dvgthontin
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(32, 39);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(477, 204);
-            this.dataGridView1.TabIndex = 0;
+            this.dvgthontin.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dvgthontin.Location = new System.Drawing.Point(32, 39);
+            this.dvgthontin.Name = "dvgthontin";
+            this.dvgthontin.RowHeadersWidth = 51;
+            this.dvgthontin.RowTemplate.Height = 24;
+            this.dvgthontin.Size = new System.Drawing.Size(477, 204);
+            this.dvgthontin.TabIndex = 0;
+            this.dvgthontin.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dvgthontin_CellContentClick);
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.comboBox2);
-            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.txtcp);
+            this.groupBox2.Controls.Add(this.txttendv);
+            this.groupBox2.Controls.Add(this.txtmadv);
+            this.groupBox2.Controls.Add(this.label3);
             this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Controls.Add(this.label1);
-            this.groupBox2.Location = new System.Drawing.Point(56, 357);
+            this.groupBox2.Location = new System.Drawing.Point(56, 329);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(540, 152);
+            this.groupBox2.Size = new System.Drawing.Size(540, 180);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "com";
             // 
-            // label1
+            // txtcp
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(32, 33);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Mã dịch vụ";
+            this.txtcp.Location = new System.Drawing.Point(98, 92);
+            this.txtcp.Name = "txtcp";
+            this.txtcp.Size = new System.Drawing.Size(100, 22);
+            this.txtcp.TabIndex = 8;
+            // 
+            // txttendv
+            // 
+            this.txttendv.Location = new System.Drawing.Point(374, 32);
+            this.txttendv.Name = "txttendv";
+            this.txttendv.Size = new System.Drawing.Size(100, 22);
+            this.txttendv.TabIndex = 7;
+            // 
+            // txtmadv
+            // 
+            this.txtmadv.Location = new System.Drawing.Point(98, 33);
+            this.txtmadv.Name = "txtmadv";
+            this.txtmadv.Size = new System.Drawing.Size(100, 22);
+            this.txtmadv.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Chi phi";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(32, 103);
+            this.label2.Location = new System.Drawing.Point(274, 33);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(76, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "Tên dịch vụ";
             // 
-            // comboBox1
+            // label1
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(167, 30);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 2;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã dịch vụ";
             // 
-            // comboBox2
+            // btnthem
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(167, 94);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 24);
-            this.comboBox2.TabIndex = 3;
+            this.btnthem.Location = new System.Drawing.Point(661, 341);
+            this.btnthem.Name = "btnthem";
+            this.btnthem.Size = new System.Drawing.Size(75, 23);
+            this.btnthem.TabIndex = 2;
+            this.btnthem.Text = "Thêm";
+            this.btnthem.UseVisualStyleBackColor = true;
+            this.btnthem.Click += new System.EventHandler(this.btnthem_Click);
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.Location = new System.Drawing.Point(661, 387);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(75, 23);
+            this.btnxoa.TabIndex = 3;
+            this.btnxoa.Text = "Xóa";
+            this.btnxoa.UseVisualStyleBackColor = true;
+            this.btnxoa.Click += new System.EventHandler(this.btnxoa_Click);
+            // 
+            // btnsua
+            // 
+            this.btnsua.Location = new System.Drawing.Point(661, 434);
+            this.btnsua.Name = "btnsua";
+            this.btnsua.Size = new System.Drawing.Size(75, 23);
+            this.btnsua.TabIndex = 4;
+            this.btnsua.Text = "Sửa";
+            this.btnsua.UseVisualStyleBackColor = true;
+            this.btnsua.Click += new System.EventHandler(this.btnsua_Click);
+            // 
+            // btnthoat
+            // 
+            this.btnthoat.Location = new System.Drawing.Point(661, 486);
+            this.btnthoat.Name = "btnthoat";
+            this.btnthoat.Size = new System.Drawing.Size(75, 23);
+            this.btnthoat.TabIndex = 5;
+            this.btnthoat.Text = "Thoát";
+            this.btnthoat.UseVisualStyleBackColor = true;
+            this.btnthoat.Click += new System.EventHandler(this.btnthoat_Click);
             // 
             // FormQLdichvu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 521);
+            this.Controls.Add(this.btnthoat);
+            this.Controls.Add(this.btnsua);
+            this.Controls.Add(this.btnxoa);
+            this.Controls.Add(this.btnthem);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "FormQLdichvu";
             this.Text = "FormQLdichvu";
+            this.Load += new System.EventHandler(this.FormQLdichvu_Load);
             this.groupBox1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dvgthontin)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
@@ -127,11 +195,17 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dvgthontin;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnthem;
+        private System.Windows.Forms.Button btnxoa;
+        private System.Windows.Forms.Button btnsua;
+        private System.Windows.Forms.Button btnthoat;
+        private System.Windows.Forms.TextBox txtcp;
+        private System.Windows.Forms.TextBox txttendv;
+        private System.Windows.Forms.TextBox txtmadv;
     }
 }
--- QuanLySanBong/FormQLdichvu.cs
+++ QuanLySanBong/FormQLdichvu.cs
@@ -7,14 +7,88 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace QuanLySanBong
 {
     public partial class FormQLdichvu : Form
     {
+        
+        SqlConnection connect;
+        SqlCommand command;
+        string str = @"Data Source=LAPTOP-8FJ7VHUC\SQLEXPRESS;Initial Catalog=QLSanBanh;Integrated Security=True";
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        DataTable table = new DataTable();
+
+        void loaddata()
+        {
+            command = connect.CreateCommand();
+            command.CommandText = "select * from DichVu"; command.ExecuteNonQuery();
+            adapter.SelectCommand = command;
+            table.Clear();
+            adapter.Fill(table);
+            dvgthontin.DataSource = table;
+
+
+        }
+
         public FormQLdichvu()
         {
             InitializeComponent();
+        }
+
+       
+
+        private void btnthoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+     
+
+        private void btnxoa_Click(object sender, EventArgs e)
+        {
+            txtmadv.ReadOnly = true;
+            command = connect.CreateCommand();
+            command.CommandText = "delete from DichVu where MaDV = ('" + txtmadv.Text + "')";
+            command.ExecuteNonQuery();
+            loaddata();
+        }
+
+        private void FormQLdichvu_Load(object sender, EventArgs e)
+        {
+            connect = new SqlConnection(str);
+            connect.Open();
+            loaddata();
+        }
+
+        private void dvgthontin_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i;
+            i = dvgthontin.CurrentRow.Index;
+            txtmadv.Text = dvgthontin.Rows[i].Cells[0].Value.ToString();
+            txttendv.Text = dvgthontin.Rows[i].Cells[1].Value.ToString();
+            txtcp.Text = dvgthontin.Rows[i].Cells[2].Value.ToString();
+            //txtmadv.Text = dvgthontin.Rows.Count.ToString();
+
+        }
+
+        private void btnthem_Click(object sender, EventArgs e)
+        {
+            
+            command = connect.CreateCommand();
+            command.CommandText = "insert into DichVu values ('"+txtmadv.Text+ "','"+txttendv.Text+ "','"+txtcp.Text+ "')";
+            command.ExecuteNonQuery();
+            loaddata();
+        }
+
+        private void btnsua_Click(object sender, EventArgs e)
+        {
+            txtmadv.ReadOnly = true;
+            command = connect.CreateCommand();
+            command.CommandText = "update DichVu set TenDV = '" + txttendv.Text + "',ChiPhi = '" + txtcp.Text + "'";
+            command.ExecuteNonQuery();
+            loaddata();
         }
     }
 }
