30344c947cac190a8669ec03f56a8cb4dd419acf
Quang Ney <quangney12602@gmail.com>
2022-10-06 18:19:43 +0700

    fMenu

--- QuanLySanBong/fMenu.Designer.cs
+++ QuanLySanBong/fMenu.Designer.cs
@@ -29,19 +29,261 @@ namespace QuanLySanBong
         /// </summary>
         private void InitializeComponent()
         {
+            this.tcMenu = new System.Windows.Forms.TabControl();
+            this.tpDoanhThu = new System.Windows.Forms.TabPage();
+            this.tpSan = new System.Windows.Forms.TabPage();
+            this.tpDichVu = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnThemSan = new System.Windows.Forms.Button();
+            this.btnSuaSan = new System.Windows.Forms.Button();
+            this.btnXoaSan = new System.Windows.Forms.Button();
+            this.btnXemSan = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tcMenu.SuspendLayout();
+            this.tpSan.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel6.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tcMenu
+            // 
+            this.tcMenu.Controls.Add(this.tpDoanhThu);
+            this.tcMenu.Controls.Add(this.tpSan);
+            this.tcMenu.Controls.Add(this.tpDichVu);
+            this.tcMenu.Location = new System.Drawing.Point(13, 13);
+            this.tcMenu.Name = "tcMenu";
+            this.tcMenu.SelectedIndex = 0;
+            this.tcMenu.Size = new System.Drawing.Size(999, 588);
+            this.tcMenu.TabIndex = 0;
+            // 
+            // tpDoanhThu
+            // 
+            this.tpDoanhThu.Location = new System.Drawing.Point(4, 29);
+            this.tpDoanhThu.Name = "tpDoanhThu";
+            this.tpDoanhThu.Padding = new System.Windows.Forms.Padding(3);
+            this.tpDoanhThu.Size = new System.Drawing.Size(991, 555);
+            this.tpDoanhThu.TabIndex = 0;
+            this.tpDoanhThu.Text = "Doanh thu";
+            this.tpDoanhThu.UseVisualStyleBackColor = true;
+            // 
+            // tpSan
+            // 
+            this.tpSan.Controls.Add(this.panel3);
+            this.tpSan.Controls.Add(this.panel2);
+            this.tpSan.Controls.Add(this.panel1);
+            this.tpSan.Location = new System.Drawing.Point(4, 29);
+            this.tpSan.Name = "tpSan";
+            this.tpSan.Padding = new System.Windows.Forms.Padding(3);
+            this.tpSan.Size = new System.Drawing.Size(991, 555);
+            this.tpSan.TabIndex = 1;
+            this.tpSan.Text = "Sân";
+            this.tpSan.UseVisualStyleBackColor = true;
+            // 
+            // tpDichVu
+            // 
+            this.tpDichVu.Location = new System.Drawing.Point(4, 29);
+            this.tpDichVu.Name = "tpDichVu";
+            this.tpDichVu.Padding = new System.Windows.Forms.Padding(3);
+            this.tpDichVu.Size = new System.Drawing.Size(991, 555);
+            this.tpDichVu.TabIndex = 2;
+            this.tpDichVu.Text = "Dịch vụ";
+            this.tpDichVu.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnXemSan);
+            this.panel1.Controls.Add(this.btnXoaSan);
+            this.panel1.Controls.Add(this.btnSuaSan);
+            this.panel1.Controls.Add(this.btnThemSan);
+            this.panel1.Location = new System.Drawing.Point(7, 7);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(642, 83);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(7, 112);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(642, 420);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.panel6);
+            this.panel3.Controls.Add(this.panel5);
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Location = new System.Drawing.Point(671, 112);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(314, 420);
+            this.panel3.TabIndex = 2;
+            // 
+            // btnThemSan
+            // 
+            this.btnThemSan.Location = new System.Drawing.Point(23, 24);
+            this.btnThemSan.Name = "btnThemSan";
+            this.btnThemSan.Size = new System.Drawing.Size(87, 44);
+            this.btnThemSan.TabIndex = 0;
+            this.btnThemSan.Text = "Thêm";
+            this.btnThemSan.UseVisualStyleBackColor = true;
+            // 
+            // btnSuaSan
+            // 
+            this.btnSuaSan.Location = new System.Drawing.Point(169, 24);
+            this.btnSuaSan.Name = "btnSuaSan";
+            this.btnSuaSan.Size = new System.Drawing.Size(87, 44);
+            this.btnSuaSan.TabIndex = 1;
+            this.btnSuaSan.Text = "Sửa";
+            this.btnSuaSan.UseVisualStyleBackColor = true;
+            // 
+            // btnXoaSan
+            // 
+            this.btnXoaSan.Location = new System.Drawing.Point(321, 24);
+            this.btnXoaSan.Name = "btnXoaSan";
+            this.btnXoaSan.Size = new System.Drawing.Size(87, 44);
+            this.btnXoaSan.TabIndex = 2;
+            this.btnXoaSan.Text = "Xóa";
+            this.btnXoaSan.UseVisualStyleBackColor = true;
+            // 
+            // btnXemSan
+            // 
+            this.btnXemSan.Location = new System.Drawing.Point(481, 24);
+            this.btnXemSan.Name = "btnXemSan";
+            this.btnXemSan.Size = new System.Drawing.Size(87, 44);
+            this.btnXemSan.TabIndex = 3;
+            this.btnXemSan.Text = "Xem";
+            this.btnXemSan.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.textBox1);
+            this.panel4.Controls.Add(this.label1);
+            this.panel4.Location = new System.Drawing.Point(25, 28);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(268, 69);
+            this.panel4.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(90, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(159, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.textBox2);
+            this.panel5.Controls.Add(this.label2);
+            this.panel5.Location = new System.Drawing.Point(25, 132);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(268, 69);
+            this.panel5.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(90, 27);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(159, 26);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Tên Sân";
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.textBox3);
+            this.panel6.Controls.Add(this.label3);
+            this.panel6.Location = new System.Drawing.Point(25, 262);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(268, 69);
+            this.panel6.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(101, 24);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(168, 26);
+            this.textBox3.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Trạng thái";
             // 
             // fMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1024, 613);
+            this.Controls.Add(this.tcMenu);
             this.Name = "fMenu";
             this.Text = "fMenu";
+            this.tcMenu.ResumeLayout(false);
+            this.tpSan.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tcMenu;
+        private System.Windows.Forms.TabPage tpDoanhThu;
+        private System.Windows.Forms.TabPage tpSan;
+        private System.Windows.Forms.TabPage tpDichVu;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnXemSan;
+        private System.Windows.Forms.Button btnXoaSan;
+        private System.Windows.Forms.Button btnSuaSan;
+        private System.Windows.Forms.Button btnThemSan;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
